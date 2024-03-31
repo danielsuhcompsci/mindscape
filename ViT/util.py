73aa74eb880c8fab1326285b6b126b8bb7d0372f
ken-myers <ken@kenmyers.io>
2024-03-30 21:44:27 -0500

    update util

--- ViT/util.py
+++ ViT/util.py
@@ -1,14 +1,33 @@
 import torch
-from torch import nn
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
-class ScaledSigmoid(nn.Module):
-    def __init__(self, bounds):
-        super().__init__()
 
-        self.bounds = bounds
+def find_mean_sd(dataset):
+    target_size = dataset[0][1].shape[0]
+    dataset_size = len(dataset)
 
-    def forward(self, x):
-        lower_bound = self.bounds[0]
-        upper_bound = self.bounds[1]
-        bounds_range = upper_bound - lower_bound
-        return lower_bound + (bounds_range * torch.sigmoid(x))
+    dataloader = DataLoader(dataset, shuffle=False, batch_size=64, num_workers=4)
+
+    mean = torch.tensor(0.0, device='cuda', dtype=torch.float64)
+    count = torch.tensor(target_size * dataset_size, device='cuda', dtype=torch.float64)
+
+    for _, targs in tqdm(dataloader, desc="Finding mean"):
+        targs = targs.to('cuda', dtype=torch.float64)
+        mean += torch.sum(targs) / count
+
+    print(f"Mean: {mean.cpu().item()}")
+
+    var = torch.tensor(0.0, device='cuda', dtype=torch.float64)
+    for _, targs in tqdm(dataloader, desc="Finding std dev"):
+        targs = targs.to('cuda', dtype=torch.float64)
+        var += torch.sum((targs - mean)**2) / count
+
+
+    std = torch.sqrt(var)
+    print(f"Std dev: {std.cpu().item()}")
+
+    mean = mean.cpu().item()
+    std = std.cpu().item()
+
+    return mean, std
