71b47f31f4fe6df6e2c7ee4e3a3429f7fa81b03b
ken-myers <ken@kenmyers.io>
2024-03-30 21:45:59 -0500

    remove bounds & sigmoid, return attentions

--- ViT/matrixvit.py
+++ ViT/matrixvit.py
@@ -1,32 +1,34 @@
 import torch
 from transformers import ViTConfig, ViTModel
 import numpy as np
-from util import ScaledSigmoid
 
 class MatrixViTModel(ViTModel):
-    #Bounds are exclusive
-    def __init__(self, output_dimensions, bounds=None, config=None):
+    def __init__(self, output_dimensions, config=None):
         if config is None:
             config = ViTConfig()
         super().__init__(config)
 
         
-        if bounds == None:
-            print("no bounds")
-            self.regression_head = torch.nn.Sequential(
-                torch.nn.Linear(config.hidden_size, np.prod(output_dimensions)), #linear projection layer
-                torch.nn.Unflatten(1, output_dimensions), #reshaping layer
-                torch.nn.Sigmoid()
-            )
-        else:
-            self.regression_head = torch.nn.Sequential(
-                torch.nn.Linear(config.hidden_size, np.prod(output_dimensions)), #linear projection layer
-                torch.nn.Unflatten(1, output_dimensions), #reshaping layer
-                ScaledSigmoid(bounds)
-            )
+        self.regression_head = torch.nn.Sequential(
+            torch.nn.Linear(config.hidden_size, np.prod(output_dimensions)), #linear projection layer
+            torch.nn.Unflatten(1, output_dimensions), #reshaping layer, not always necessary
+        )
 
+    
     def forward(self, x, **kwargs):
         outputs = super().forward(x, **kwargs)
-        class_token = outputs.last_hidden_state[:, 0, :]
+        
+        class_token = outputs.last_hidden_state[:, 0, :]  # class token is first token
         regression_output = self.regression_head(class_token)
-        return regression_output
+        
+        # include attentions if requested
+        if 'return_dict' in kwargs and kwargs['return_dict'] == True:
+            res = {
+                "regression_output": regression_output
+            }
+
+            if 'output_attentions' in kwargs and kwargs['output_attentions'] == True:
+                res['attentions'] = outputs.attentions
+            return res
+        else:
+            return regression_output
