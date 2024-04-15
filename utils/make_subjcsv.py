c536428dbc74a6c451eef34531b9125016bf4011
dustyCheese72 <daniel-herrera2006@hotmail.com>
2024-04-15 21:35:09 +0000

    Added binarized betas

--- utils/make_subjcsv.py
+++ utils/make_subjcsv.py
@@ -13,27 +13,35 @@ opt = parser.parse_args()
 
 subject = opt.subject
 nsd_dir = opt.nsddir
-out_dir = opt.o
+out_dir = opt.output
+betas = np.load(os.path.join(nsd_dir, f'nsddata_betas/masked/subj{subject:02n}.npy'))
+headers = []
+
+
+for i in range(30000):
+    threshold = np.percentile(betas[i], 95)
+    betas[i] = betas[i] > threshold
+
+for i in range(betas.shape[1]):
+    headers = np.concatenate((headers, [f'voxel-{i}']))
+
+csv = pd.DataFrame(np.asarray(betas, dtype=np.int32), columns=headers)
+
+viewedImgIds = pd.read_csv(os.path.join(
+    nsd_dir, f'nsddata/ppdata/subj{subject:02n}/behav/responses.tsv'), delimiter='\t', usecols=[4])['73KID']
+viewedImgIds = (viewedImgIds - 1).transform(pd.read_csv(os.path.join(
+    nsd_dir, 'nsddata/experiments/nsd/nsd_stim_info_merged.csv'), usecols=[1])['cocoId'].at.__getitem__)
 
 cocoval = COCO(os.path.join(nsd_dir, 'nsddata_stimuli/stimuli/nsd/annotations/instances_val2017.json'))
 cocotrain = COCO(os.path.join(nsd_dir, 'nsddata_stimuli/stimuli/nsd/annotations/instances_train2017.json'))
 cat_ids = cocoval.getCatIds()
 categories = json_normalize(cocoval.loadCats(cat_ids))
 
-csv = pd.DataFrame(np.zeros((3000, 0)))
-viewedImgIds = pd.read_csv(os.path.join(
-    nsd_dir, f'nsddata/ppdata/subj{subject:02n}/behav/responses.tsv'), delimiter='\t', usecols=[4])['73KID']
-viewedImgIds = (viewedImgIds - 1).transform(pd.read_csv(os.path.join(
-    nsd_dir, 'nsddata/experiments/nsd/nsd_stim_info_merged.csv'), usecols=[1])['cocoId'].at.__getitem__)
-
 for cat_id in cat_ids:
-    catImgIds = np.concatenate(cocoval.getImgIds(catIds=[cat_id]), cocotrain.getImgIds(catIds=[cat_id]))
+    catImgIds = np.concatenate((cocoval.getImgIds(catIds=[cat_id]), cocotrain.getImgIds(catIds=[cat_id])))
     csv.insert(csv.shape[1], categories[categories['id']==cat_id]['name'].iloc[0], viewedImgIds.isin(catImgIds) * 1)
 
-#dummy values
-for i in range(5246):
-    csv.insert(csv.shape[1], 'voxel-{i}', np.random.randint(2, size=(3000, 1)))
-csv.insert(csv.shape[1], 'x-coord', np.random.randint(2, size=(3000,1)))
-csv.insert(csv.shape[1], 'y-coord', np.random.randint(2, size=(3000,1)))
+csv.insert(csv.shape[1], 'x-coord', np.random.randint(2, size=(30000,1)))
+csv.insert(csv.shape[1], 'y-coord', np.random.randint(2, size=(30000,1)))
 
 csv.to_csv(os.path.join(out_dir, 'subj{subject:02n}.csv'))
