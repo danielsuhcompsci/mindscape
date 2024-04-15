40bb1dd9a672cc36c90fa0ce276757a0098d96a1
Ken Myers <ken@kenmyers.io>
2024-04-15 03:53:26 +0000

    baseline flash attention

--- ViT/matrixvit.py
+++ ViT/matrixvit.py
@@ -1,34 +1,13 @@
 import torch
-from transformers import ViTConfig, ViTModel
+from vit_pytorch.simple_flash_attn_vit import SimpleViT
 import numpy as np
 
-class MatrixViTModel(ViTModel):
-    def __init__(self, output_dimensions, config=None):
-        if config is None:
-            config = ViTConfig()
-        super().__init__(config)
-
+class MatrixViTModel(SimpleViT):
+    def __init__(self, *, output_dimensions, dim, **kwargs):
         
-        self.regression_head = torch.nn.Sequential(
-            torch.nn.Linear(config.hidden_size, np.prod(output_dimensions)), #linear projection layer
+        super().__init__(num_classes=1, dim=dim, **kwargs)
+
+        self.linear_head = torch.nn.Sequential(
+            torch.nn.Linear(dim, np.prod(output_dimensions)), #linear projection layer
             torch.nn.Unflatten(1, output_dimensions), #reshaping layer, not always necessary
         )
-
-    
-    def forward(self, x, **kwargs):
-        outputs = super().forward(x, **kwargs)
-        
-        class_token = outputs.last_hidden_state[:, 0, :]  # class token is first token
-        regression_output = self.regression_head(class_token)
-        
-        # include attentions if requested
-        if 'return_dict' in kwargs and kwargs['return_dict'] == True:
-            res = {
-                "regression_output": regression_output
-            }
-
-            if 'output_attentions' in kwargs and kwargs['output_attentions'] == True:
-                res['attentions'] = outputs.attentions
-            return res
-        else:
-            return regression_output
