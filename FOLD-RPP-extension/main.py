12ecf808ff6b01cbc19442095b042fd32db7284b
IsaacP <isaacphilo1@gmail.com>
2024-04-16 12:42:25 -0500

    Removed unnecessary stuff
    I removed the commented-out version of the old code and added a toggle for the diagnostic print of the first line

--- FOLD-RPP-extension/main.py
+++ FOLD-RPP-extension/main.py
@@ -4,75 +4,6 @@ from brainDataset import brainVoxels
 from timeit import default_timer as timer
 from datetime import timedelta
 
-
-# def main():
-#     load_start = timer()
-#
-#     # model, data = acute()
-#     # model, data = autism()
-#     # model, data = breastw()
-#     # model, data = cars()
-#     # model, data = credit()
-#     # model, data = heart()
-#     model, data = kidney()
-#     # model, data = krkp()
-#     # model, data = mushroom()
-#     # model, data = sonar()
-#     # model, data = voting()
-#     # model, data = ecoli()
-#     # model, data = ionosphere()
-#     # model, data = wine()
-#     # model, data = adult()
-#     # model, data = credit_card()
-#     # model, data = rain()
-#     # model, data = heloc()
-#     # model, data = parkison()
-#
-#     load_end = timer()
-#     print('% load data costs: ', timedelta(seconds=load_end - load_start), '\n')
-#
-#     data_train, data_test = split_data(data, ratio=0.8, rand=True)
-#
-#     start = timer()
-#     model.fit(data_train)
-#     end = timer()
-#
-#     for r in model.asp():
-#         print(r)
-#
-#     ys_test_hat = model.predict(data_test)
-#     ys_test = [x['label'] for x in data_test]
-#     acc, p, r, f1 = get_scores(ys_test_hat, ys_test)
-#     print('% acc', round(acc, 3), 'p', round(p, 3), 'r', round(r, 3), 'f1', round(f1, 3))
-#     n_rules, n_preds = len(model.flat_rules), num_predicates(model.flat_rules)
-#     print('% #rules', n_rules, '#preds', n_preds)
-#     print('% foldrpp costs: ', timedelta(seconds=end - start), '\n')
-#
-#     for x in data_test[:10]:
-#         for r in model.proof_rules(x):
-#             print(r)
-#         for r in model.proof_trees(x):
-#             print(r)
-#
-#     from foldrpp import save_model_to_file, load_model_from_file
-#     save_model_to_file(model, 'model.txt')
-#     saved_model = load_model_from_file('model.txt')
-#
-#     ys_test_hat = saved_model.predict(data_test)
-#     ys_test = [x['label'] for x in data_test]
-#     acc, p, r, f1 = get_scores(ys_test_hat, ys_test)
-#     print('% acc', round(acc, 3), 'p', round(p, 3), 'r', round(r, 3), 'f1', round(f1, 3))
-#
-#     # for x in data_test[:10]:
-#     #     for r in saved_model.proof_rules(x):
-#     #         print(r)
-#     #     for r in saved_model.proof_trees(x):
-#     #         print(r)
-#
-#
-# if __name__ == '__main__':
-#     main()
-
 def main():
     categories = ["person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
                   "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
@@ -86,30 +17,8 @@ def main():
 
     for category in categories:
         print(f"Beginning analysis on category {category}")
-        model, data = brainVoxels(category, '..\FOLDdata\subj01.csv')
-        print(data[0])
         load_start = timer()
-
-        # model, data = acute()
-        # model, data = autism()
-        # model, data = breastw()
-        # model, data = cars()
-        # model, data = credit()
-        # model, data = heart()
-        # model, data = kidney()
-        # model, data = krkp()
-        # model, data = mushroom()
-        # model, data = sonar()
-        # model, data = voting()
-        # model, data = ecoli()
-        # model, data = ionosphere()
-        # model, data = wine()
-        # model, data = adult()
-        # model, data = credit_card()
-        # model, data = rain()
-        # model, data = heloc()
-        # model, data = parkison()
-
+        model, data = brainVoxels(category, '..\FOLDdata\subj01.csv', False)
         load_end = timer()
         print('% load data costs: ', timedelta(seconds=load_end - load_start), '\n')
 
