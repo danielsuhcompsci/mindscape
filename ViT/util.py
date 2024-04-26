e7cb90fb464ef129ef778a9f86812d62fe07cdb3
Ken Myers <ken@kenmyers.io>
2024-04-26 18:54:00 +0000

    better norm and dataset filtering

--- ViT/util.py
+++ ViT/util.py
@@ -3,6 +3,7 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 import numpy as np
 import random
+from torch.utils.data import Dataset
 
 def set_seed(SEED=47):
     torch.manual_seed(SEED)
@@ -10,6 +11,43 @@ def set_seed(SEED=47):
     random.seed(SEED)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(SEED)  # For CUDA devices
+        
+
+def normalize_per_voxel(voxels, means, std_devs):
+    return (voxels - means) / std_devs
+
+def mean_sd_map(voxels, device = None):
+    
+    if device is None:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    voxels.to(device)
+
+    means = torch.mean(voxels, dim=0)
+    std_devs = torch.std(voxels, dim=0)
+    
+    return torch.stack((means, std_devs))
+
+
+class FilteredDataset(Dataset):
+
+    def __init__(self, dataset, indices, max_count = None):
+        self.dataset = dataset
+        self.indices = indices
+        self.max_count = max_count
+
+        if self.max_count is not None:
+            if len(self.indices) > self.max_count:
+                self.indices = self.indices[:self.max_count]
+    
+    def __len__(self):
+        return len(self.indices)
+    
+    def __getitem__(self, idx):
+        idx = self.indices[idx]
+        return self.dataset[idx]
+
+
 
 
 def find_mean_sd(dataset):
