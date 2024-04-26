54616b2268038ddd2029cdfbc7d2cac7ce50db4b
Ken Myers <ken@kenmyers.io>
2024-04-26 20:47:12 +0000

    extend matrixvit

--- ViT/per-voxel-matrix-vit.py
+++ ViT/per-voxel-matrix-vit.py
@@ -1,27 +1,14 @@
+from matrixvit import MatrixViTModel
 import torch
-from vit_pytorch.simple_flash_attn_vit import SimpleViT, posemb_sincos_2d, Transformer as FlashTransformer
 import numpy as np
-from einops import rearrange
-from einops.layers.torch import Rearrange
+from vit_pytorch.simple_flash_attn_vit import posemb_sincos_2d
 
-class FlashDropoutTransformer(FlashTransformer):
-    def __init__(self, dim, depth, heads, dim_head, mlp_dim, use_flash, drop_rate = 0.2):
-        super().__init__(dim=dim, depth=depth, heads=heads, dim_head = dim_head, mlp_dim = mlp_dim, use_flash=use_flash)
-        self.dropout = torch.nn.Dropout(drop_rate)
-    
-    def forward(self, x):
-        for attn, ff in self.layers:
-            x = attn(x) + x  # apply attention and add residual
-            x = self.dropout(x)  # apply dropout after residual (optional location)
-            x = ff(x) + x  # apply feed-forward and add residual
-            x = self.dropout(x)  # apply dropout after residual (standard location)
-        return x
-
-
-class PerVoxelMatrixViTModel(SimpleViT):
+class PerVoxelMatrixViTModel(MatrixViTModel):
     def __init__(self, *, output_dimensions, dim, depth, heads, mlp_dim, dim_head=64, use_flash=True, drop_rate=0.2, **kwargs):
+        super.__init__(output_dimensions=output_dimensions, dim=dim, depth=depth, heads=heads, mlp_dim=mlp_dim, dim_head=dim_head, use_flash=use_flash, drop_rate=drop_rate, **kwargs)
         
-        super().__init__(num_classes=1, dim=dim, depth=depth, heads=heads, dim_head=dim_head, mlp_dim=mlp_dim, use_flash=use_flash, **kwargs)
+        self.class_token = None
+        self.linear_head = None
 
         #class token for each voxel
         num_voxels = np.prod(output_dimensions)
@@ -34,9 +21,7 @@ class PerVoxelMatrixViTModel(SimpleViT):
             [nn.Linear(dim, 1) for _ in range(num_voxels)]
         )
 
-        self.transformer = FlashDropoutTransformer(dim, depth, heads, dim_head, mlp_dim, use_flash, drop_rate)
-
-    def forward(self, img):
+    def forward(x):
         *_, h, w, dtype = *img.shape, img.dtype
 
         x = self.to_patch_embedding(img)
@@ -50,10 +35,9 @@ class PerVoxelMatrixViTModel(SimpleViT):
 
         x = torch.cat((class_tokens, x), dim=1)
 
-
         x = self.transformer(x)
         
-        x = x[:, 0] # extract class token
+        x = x[:, 0] # extract class tokens
 
         x = self.dropout(x)
         x = self.to_latent(x)
